ed2ec86e6e15e5c900b16e68d04a483788ecad84
YASHAVIKA SINGH <YASHAVIKA.SINGH@GMAIL.COM>
2024-06-12 18:26:58 +0000

    initial commit, added json file, decoder, assets

---
+++ Moonshot/astronaut.swift
@@ -0,0 +1,18 @@
+//
+//  astronaut.swift
+//  Moonshot
+//
+//  Created by Yashavika Singh on 12.06.24.
+//
+
+import Foundation
+
+
+struct Astronaut: Codable, Identifiable{
+    let id: String
+    let name: String
+    let description: String
+}
+
+
+ 
